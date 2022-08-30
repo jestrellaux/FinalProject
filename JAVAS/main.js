c3fe01b40fb16f534dbda496e9e3a8c1ff3c5175
jestrellaux <101490521+jestrellaux@users.noreply.github.com>
2022-08-29 22:07:04 -0400

    pm

---
+++ JAVAS/main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    alert("Thank you for downloading my resume");
+    console.log("Hello world!");}
+  
+function imageShow(){
+    document.getElementById('image').style.display = "block";       
+    }
